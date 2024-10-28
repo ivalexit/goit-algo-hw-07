b9f19a6390fa34e5e8df1dd366bee8b039279a12
ivalexit <ivalextar@gmail.com>
2024-10-28 16:43:05 +0100

    hw7_1 find max value

---
+++ hw7_1.py
@@ -0,0 +1,83 @@
+
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+def insert(root, key):
+    if not root:
+        return AVLNode(key)
+    
+    if key < root.key:
+        root.left = insert(root.left, key)
+    elif key > root.key:
+        root.right = insert(root.right, key)
+    else:
+        return root
+    
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+    balance = get_balance(root)
+
+    if balance > 1:
+        if key < root.left.key:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+        
+    if balance < -1:
+        if key > root.right.key:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+        
+    return root
+
+def get_height(node):
+    if not not node:
+        return 0
+    return node.height
+
+def get_balance(node):
+    if not node:
+        return 0
+    return get_height(node.left) - get_height(node.right)
+
+def left_rotate(z):
+    y = z.right
+    T2 = y.left
+
+    y.left = z
+    z.right = T2
+
+    z.height = 1 + max(get_height(z.left), get_height(z.right))
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+
+    return y
+
+def right_rotate(y):
+    x = y.left
+    T3 = x.right
+
+    x.right = y
+    y.left = T3
+
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+    x.height = 1 + max(get_height(x.left), get_height(x.right))
+
+    return x
+
+def find_max_value(root):
+    current = root        #start with root
+    while current.right:   #iteration to the right because there are bigger values
+        current = current.right #replace 'current' with its right descendent
+    return current.key if current else None #return key value as the biggest or None if tree is empty
+
+root = 50
+keys = [5, 40, 15, 79, 46, 28, 95, 37, 17, 50, 68, 43, 86, 31, 83]
+
+
+print("Max value in the AVL-tree is:", find_max_value(root))
