d52bd4f219e3e0e977484470ab2dfbe0a5d93729
ivalexit <ivalextar@gmail.com>
2024-10-28 16:58:24 +0100

    hw7_2 min value

---
+++ hw7_2.py
@@ -0,0 +1,84 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+def insert(root, key):
+    if not root:
+        return AVLNode(key)
+    
+    if key < root.key:
+        root.left = insert(root.left, key)
+    elif key > root.key:
+        root.right = insert(root.right, key)
+    else:
+        return root
+    
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+    balance = get_balance(root)
+
+    if balance > 1:
+        if key < root.left.key:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+        
+    if balance < -1:
+        if key > root.right.key:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+        
+    return root
+
+def get_height(node):
+    if not node:
+        return 0
+    return node.height
+
+def get_balance(node):
+    if not node:
+        return 0
+    return get_height(node.left) - get_height(node.right)
+
+def left_rotate(z):
+    y = z.right
+    T2 = y.left
+
+    y.left = z
+    z.right = T2
+
+    z.height = 1 + max(get_height(z.left), get_height(z.right))
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+
+    return y
+
+def right_rotate(y):
+    x = y.left
+    T3 = x.right
+
+    x.right = y
+    y.left = T3
+
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+    x.height = 1 + max(get_height(x.left), get_height(x.right))
+
+    return x
+
+def find_min_value(root):
+    current = root #start with root
+    while current.left: #iterration to the left subtree
+        current = current.left #replacing current with its left descendent
+    return current.key if current else None #returning the smallest value or None
+
+root = None
+keys = [5, 40, 15, 79, 46, 28, 95, 37, 17, 50, 68, 43, 86, 31, 83]
+
+for key in keys:
+    root = insert(root, key)
+
+print("Min value in the AVL-tree is:", find_min_value(root))
